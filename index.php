fc2045a5710058f1faefb83c5a166539ee2bc897
Alpesh Patil <alpesh.softanic@gmail.com>
2021-06-30 20:48:48 +0530

    Add index file

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+include './header.php';
+?>
+<!-- Main content -->
+<div class="content-wrapper">
+
+    <!-- Page header -->
+    <div class="page-header">
+
+    </div>
+    <!-- /page header -->
+
+    <!-- Content area -->
+    <div class="content">
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-flat">
+                    <div class="panel-heading">
+                        <center><h1 class="panel-title" style="color: red;"><b>
+                                    Welcome To Meena Hospital<br>
+
+                                </b></h1></center><br><br>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+
+        <?php
+        include './footer.php';
+        ?>
+
